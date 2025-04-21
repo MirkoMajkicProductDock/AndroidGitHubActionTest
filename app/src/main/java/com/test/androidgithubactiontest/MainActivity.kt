95c32fe99dea2e9f3af488ad6697dd40a14ff16c
MirkoMajkicProductDock <mirko.majkic@productdock.com>
2025-04-21 15:07:24 +0200

    Add build as action, separate from workflow

--- app/src/main/java/com/test/androidgithubactiontest/MainActivity.kt
+++ app/src/main/java/com/test/androidgithubactiontest/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.test.androidgithubactiontest.ui.theme.AndroidGitHubActionTestTheme
 
@@ -21,7 +24,8 @@ class MainActivity : ComponentActivity() {
             AndroidGitHubActionTestTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Visitor",
+                        version = BuildConfig.VERSION_NAME,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +35,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String, version: String, modifier: Modifier = Modifier) {
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Hello $name, \ncurrent version is $version!",
+            modifier = modifier,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidGitHubActionTestTheme {
-        Greeting("Android")
+        Greeting("Android", "1.0.0")
     }
 }
